fbd6f0ddba34f743aa580463aed74fb75acf6170
Jake McCrary <jake@jakemccrary.com>
2014-02-16 14:55:54 -0600

    lein-release plugin: preparing 1.2.2 release

--- autoexpect/project.clj
+++ autoexpect/project.clj
@@ -1,4 +1,4 @@
-(defproject lein-autoexpect "1.2.2-SNAPSHOT"
+(defproject lein-autoexpect "1.2.2"
   :description "Automatically run expecations when a source file changes"
   :url "https://github.com/jakemcc/lein-autoexpect"
   :developer "Jake McCrary"
