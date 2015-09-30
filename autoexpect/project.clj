f1a84860e028e92919c320f76562a6122ab3069a
Jake McCrary <jake@jakemccrary.com>
2015-09-30 08:57:55 -0500

    Starting development on lein-autoexpect 1.7.1-SNAPSHOT

--- autoexpect/project.clj
+++ autoexpect/project.clj
@@ -1,4 +1,4 @@
-(defproject lein-autoexpect "1.7.0"
+(defproject lein-autoexpect "1.7.1-SNAPSHOT"
   :description "Automatically run expecations when a source file changes"
   :url "https://github.com/jakemcc/lein-autoexpect"
   :developer "Jake McCrary"
