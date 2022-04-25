599ff16503c89a5c657c675d76ecc2201250715e
Jake McCrary <jake@jakemccrary.com>
2022-04-25 09:45:17 -0500

    Preparing for release lein-autoexpect 1.10.0

--- autoexpect/project.clj
+++ autoexpect/project.clj
@@ -1,4 +1,4 @@
-(defproject lein-autoexpect "1.10.0-SNAPSHOT"
+(defproject lein-autoexpect "1.10.0"
   :description "Automatically run expecations when a source file changes"
   :url "https://github.com/jakemcc/lein-autoexpect"
   :developer "Jake McCrary"
